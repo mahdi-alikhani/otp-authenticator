d89e593323cf59bf12d728f21933ed720bc40188
mahdi.alikhani <mahdi.alikhani@partsoftware.com>
2023-02-06 14:13:02 +0330

    Register evenet service provider

--- src/OTPAuthenticatorServiceProvider.php
+++ src/OTPAuthenticatorServiceProvider.php
@@ -4,11 +4,14 @@ namespace Mahdialikhani\OtpAuthenticator;
 
 use Illuminate\Support\ServiceProvider;
 use Mahdialikhani\OtpAuthenticator\Console\InstallCommand;
+use Mahdialikhani\OtpAuthenticator\Providers\EventServiceProvider;
 
 class OTPAuthenticatorServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->app->register(EventServiceProvider::class);
+
         $this->mergeConfigFrom(
             __DIR__ . '/../config/otpauthenticator.php',
             'otpauthenticator'
